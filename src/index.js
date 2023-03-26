a0ff093a9fcf71e6674f20fc85d8afaa55a47bcf
Wycliffe <wycliffekiprono4@gmail.com>
2023-03-26 10:13:35 +0300

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
+  //target the DOM element we want
+    const inputForm = document.querySelector('form')
+    //add eventListener to the form with two arguments
+    inputForm.addEventListener('submit', (event) => {
+
+        //override form's behavior; stops page from refreshing
+        event.preventDefault()
+        
+        //event.target.children[1].
+        const input = document.querySelector('input#searchByID')
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(resp => resp.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4')
+            const summary = document.querySelector('section#movieDetails p')
+            
+            title.innerText = data.title
+            summary.innerText = data.summary
+        })
+
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
